be1bbd472baf02fc1eee07140625062384d88b93
Pam Hamrick <leahamr@yahoo.com>
2020-07-14 19:32:38 -0500

    changed titles table and dept_emp table keys

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -6,16 +6,28 @@ CREATE TABLE departments (
 	UNIQUE (dept_name)
 );
 SELECT * FROM departments;
+
 CREATE TABLE employees (
 	emp_no INT NOT NULL,
 	birth_date DATE NOT NULL,
-	first_name	VARCHAR NOT NULL,
+	first_name VARCHAR NOT NULL,
 	last_name VARCHAR NOT NULL,
 	gender VARCHAR NOT NULL,
 	hire_date DATE NOT NULL,
 	PRIMARY KEY (emp_no)
 );
 SELECT * FROM employees;
+
+CREATE TABLE salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no)
+);
+SELECT * FROM salaries;
+
 CREATE TABLE dept_manager (
 	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
@@ -26,30 +38,23 @@ CREATE TABLE dept_manager (
 	PRIMARY KEY (emp_no, dept_no)
 );
 SELECT * FROM dept_manager;
-CREATE TABLE salaries (
+CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
-	salary INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
-);
-SELECT * FROM salaries;
-CREATE TABLE managers (
-	dept_no VARCHAR(4) NOT NULL,
-	emp_no INT NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (dept_no)
+	PRIMARY KEY (emp_no, dept_no)
 );
-SELECT * FROM managers;
+SELECT * FROM dept_emp;
+
 CREATE TABLE titles (
 	emp_no INT  NOT NULL,
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
-	PRIMARY KEY (emp_no, title, to_date)
+	PRIMARY KEY (emp_no, title, from_date)
 );
 SELECT * FROM titles;
